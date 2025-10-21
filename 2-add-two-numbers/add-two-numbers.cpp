4b953fd5e1ffde8f9902c1555bf93be7c350bbda
Priyanshu yadav <124013630+impriyanshu8@users.noreply.github.com>
2025-10-21 10:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (13.77%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,25 +11,44 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode ();
-        ListNode* temp = dummy;
         int carry = 0;
-        while((l1 != NULL)||(l2 != NULL)||(carry)){
-            int sum = 0;
-            if(l1 != NULL){
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if(l2 != NULL){
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            sum += carry;
-            carry = sum / 10;
-            ListNode* Node = new ListNode(sum % 10);
-            temp -> next = Node;
-            temp = temp->next;
+        int n = l1->val + l2->val;
+        carry = (n/10);
+        ListNode* sumHead = new ListNode(n%10);
+        ListNode* sum = sumHead;
+        l1 = l1->next;
+        l2 = l2->next;
+        while(l1 != NULL && l2 != NULL){
+            n = l1->val + l2->val + carry;
+            carry = n/10;
+            ListNode* temp = new ListNode(n%10);
+            sum -> next = temp;
+            sum = sum -> next;
+            l1 = l1-> next;
+            l2 = l2-> next;
         }
-        return dummy->next;
+        while(l1 != NULL){
+            n = l1->val + carry;
+             ListNode* temp = new ListNode(n%10);
+            carry = n/10;
+            sum->next = temp;
+            sum = sum->next;
+            l1 = l1->next;
+        }
+        while(l2 != NULL){
+            n = l2->val + carry;
+            ListNode* temp = new ListNode(n%10);
+            carry = n/10;
+            sum->next = temp;
+            sum = sum->next;
+            l2 = l2->next;
+        }
+        if((l1 == NULL && l2 == NULL) && carry > 0){
+            ListNode* temp = new ListNode(carry);
+            sum->next = temp;
+            sum = sum->next;
+            
+        }
+        return sumHead;
     }
 };
