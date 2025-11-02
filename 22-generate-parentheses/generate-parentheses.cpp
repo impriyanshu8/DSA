eddcc8a87512f32255a1f869b63997752ab6322a
Priyanshu yadav <124013630+impriyanshu8@users.noreply.github.com>
2025-11-02 13:23:59 +0530

    Time: 3 ms (71.54%) | Memory: 15.7 MB (35.46%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,24 +1,5 @@
 class Solution {
 public:
-    // bool isValid(string s) {
-    //     int balance = 0;
-    //     for (char c : s) {
-    //         if (c == '(') balance++;
-    //         else balance--;
-    //         if (balance < 0) return false; // closing before opening
-    //     }
-    //     return balance == 0;
-    // }
-
-    // void generateAll(string curr, int n, vector<string> &ans) {
-    //     if (curr.length() == 2 * n) {
-    //         if (isValid(curr)) ans.push_back(curr);
-    //         return; // stop recursion here
-    //     }
-
-    //     generateAll(curr + '(', n, ans);
-    //     generateAll(curr + ')', n, ans);
-    // }
     void generate(string curr, int open, int close, int n, vector<string> &ans){
         if(curr.length()== 2*n){
             ans.push_back(curr);
