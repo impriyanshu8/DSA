e5c515bcb7d8d99cde53de223f5efa03c4db6b01
Priyanshu yadav <124013630+impriyanshu8@users.noreply.github.com>
2025-11-02 13:23:36 +0530

    Time: 4 ms (34.97%) | Memory: 15.7 MB (35.46%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,28 +1,36 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        int balance = 0;
-        for (char c : s) {
-            if (c == '(') balance++;
-            else balance--;
-            if (balance < 0) return false; // closing before opening
-        }
-        return balance == 0;
-    }
+    // bool isValid(string s) {
+    //     int balance = 0;
+    //     for (char c : s) {
+    //         if (c == '(') balance++;
+    //         else balance--;
+    //         if (balance < 0) return false; // closing before opening
+    //     }
+    //     return balance == 0;
+    // }
 
-    void generateAll(string curr, int n, vector<string> &ans) {
-        if (curr.length() == 2 * n) {
-            if (isValid(curr)) ans.push_back(curr);
-            return; // stop recursion here
-        }
+    // void generateAll(string curr, int n, vector<string> &ans) {
+    //     if (curr.length() == 2 * n) {
+    //         if (isValid(curr)) ans.push_back(curr);
+    //         return; // stop recursion here
+    //     }
 
-        generateAll(curr + '(', n, ans);
-        generateAll(curr + ')', n, ans);
+    //     generateAll(curr + '(', n, ans);
+    //     generateAll(curr + ')', n, ans);
+    // }
+    void generate(string curr, int open, int close, int n, vector<string> &ans){
+        if(curr.length()== 2*n){
+            ans.push_back(curr);
+            return;
+        }
+        if(open<n) generate(curr+'(',open+1, close, n, ans);
+        if(close<open) generate(curr+')',open, close+1, n, ans);
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
-        generateAll("", n, ans);
+        generate("",0,0,n, ans);
         return ans;
     }
 };
